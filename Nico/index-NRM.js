4106cc78a56899af27c7fef69dc0f46a8cd7f88a
NicoRedondoo <redondonico9@gmail.com>
2024-02-20 10:10:30 +0100

    n

--- Nico/index-NRM.js
+++ Nico/index-NRM.js
@@ -1,21 +1,33 @@
 const fs = require('fs');
 
-const data = fs.readFileSync('ufc_dataset.csv', 'utf8');
+const data2 = fs.readFileSync('ufc_dataset_copy.csv', 'utf8');
 
-function contarCiudades(dataset) {
-    
-    const lineas = dataset.split('\n');
-    
-    const ciudadesUnicas = new Set();
-    
-    lineas.slice(1).forEach((linea) => {
-        const ciudad = linea.split(';')[0].trim();
-        ciudadesUnicas.add(ciudad);
-    });
-    
-    // Retornar el tamaño del conjunto, que representa el número de ciudades únicas
-    return ciudadesUnicas.size;
-}
+const data = [
+    "Austin Texas USA\tArman Tsarukyan\tBeneil Dariush\t1.0\t0.0\t8.0\t2.0\t0.0\t0.0\t0.0\t0.0\tLightweight\tKO/TKO Punch\t1.0\t1:04\tUFC Fight Night\t2-Dec-23\tArman Tsarukyan",
+    "Austin Texas USA\tJalin Turner\tBobby Green\t1.0\t0.0\t33.0\t15.0\t0.0\t0.0\t0.0\t0.0\tLightweight\tKO/TKO Punches\t1.0\t2:49\tUFC Fight Night\t2-Dec-23\tJalin Turner",
+    "Austin Texas USA\tDeiveson Figueiredo\tRob Font\t0.0\t0.0\t45.0\t46.0\t4.0\t0.0\t0.0\t0.0\tBantamweight\tU-DEC\t3.0\t5:00\tUFC Fight Night\t2-Dec-23\tDeiveson Figueiredo",
+    "Austin Texas USA\tSean Brady\tKelvin Gastelum\t0.0\t0.0\t14.0\t18.0\t5.0\t0.0\t3.0\t0.0\tWelterweight\tSUB Kimura\t3.0\t1:43\tUFC Fight Night\t2-Dec-23\tSean Brady",
+    "Austin Texas USA\tJoaquim Silva\tClay Guida\t0.0\t0.0\t46.0\t43.0\t2.0\t2.0\t2.0\t0.0\tLightweight\tU-DEC\t3.0\t5:00\tUFC Fight Night\t2-Dec-23\tJoaquim Silva",
+    "Austin Texas USA\tDustin Stoltzfus\tPunahele Soriano\t1.0\t0.0\t64.0\t31.0\t4.0\t0.0\t1.0\t0.0\tMiddleweight\tSUB Rear Naked Choke\t2.0\t4:10\tUFC Fight Night\t2-Dec-23\tDustin Stoltzfus",
+    "Austin Texas USA\tMiesha Tate\tJulia Avila\t0.0\t0.0\t32.0\t3.0\t4.0\t0.0\t1.0\t0.0\tWomen's Bantamweight\tSUB Rear Naked Choke\t3.0\t1:15\tUFC Fight Night\t2-Dec-23\tMiesha Tate",
+    "Austin Texas USA\tCody Brundage\tZach Reese\t0.0\t0.0\t9.0\t4.0\t1.0\t0.0\t0.0\t1.0\tMiddleweight\tKO/TKO Slam\t1.0\t1:49\tUFC Fight Night\t2-Dec-23\tCody Brundage",
+    "Austin Texas USA\tDrakkar Klose\tJoe Solecki\t0.0\t0.0\t2.0\t1.0\t0.0\t1.0\t0.0\t0.0\tLightweight\tKO/TKO Slam\t1.0\t1:41\tUFC Fight Night\t2-Dec-23\tDrakkar Klose",
+    "Austin Texas USA\tRodolfo Bellato\tIhor Potieria\t0.0\t1.0\t80.0\t73.0\t1.0\t0.0\t0.0\t0.0\tLight Heavyweight\tKO/TKO Punches\t2.0\t4:17\tUFC Fight Night\t2-Dec-23\tRodolfo Bellato",
+    "Austin Texas USA\tJared Gooden\tWellington Turman\t0.0\t0.0\t53.0\t37.0\t1.0\t1.0\t1.0\t0.0\tWelterweight\tSUB Rear Naked Choke\t2.0\t1:11\tUFC Fight Night\t2-Dec-23\tJared Gooden"
+];
 
-const numeroCiudades = contarCiudades(data);
-console.log('En el dataset hay', numeroCiudades, ' ciudades distintas.');
+
+let totalSegundos = 0;
+
+data.forEach(linea => {
+    const tiempo = linea.split('\t')[14].split(':'); // Obtiene el tiempo de la columna 15 (índice 14)
+    const minutos = parseInt(tiempo[0], 10);
+    const segundos = parseInt(tiempo[1], 10);
+    totalSegundos += minutos * 60 + segundos;
+});
+
+const mediaSegundos = totalSegundos / data.length;
+const mediaMinutos = Math.floor(mediaSegundos / 60);
+const mediaSegundosRestantes = Math.round(mediaSegundos % 60);
+
+console.log('La media de tiempo empleado en cada pelea es de ', mediaMinutos, ' minutos y ', mediaSegundosRestantes, ' segundos.');
