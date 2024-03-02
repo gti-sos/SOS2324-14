2eed17b65d29ccd561faa29637f4c173d802a342
Víctor Escalera García <vicescgar@us.es>
2024-03-02 19:25:30 +0100

    api victor

--- api-VEG.js
+++ api-VEG.js
@@ -26,24 +26,24 @@ function api_VEG(app) {
                 };
                 data_VEG.push(nuevoDato);
             }
-            res.status(201).send("Se han creado 10 datos o más en el array de NodeJS.");
+            res.sendStatus(201, "Created");
         } else {
-            res.status(409).send("El array de NodeJS ya contiene datos. No se han creado nuevos datos.");
+            res.sendStatus(409, "Conflict");
         }
     });
 
     //GET Un recurso inexistente
     app.get(API_BASE + "/datos-youtube", (req, res) => {
-        res.status(404).json({ error: "No se encuentra el recurso especificado" });
+        res.sendStatus(404, "Not Found");
     });
 
     // GET para obtener los datos relacionados con el país "Albania"
     app.get(API_BASE + "/youtube-trends/Albania", (req, res) => {
         const datosAlbania = data_VEG.filter(item => item.country === "Albania");
-    
+
         // Verificar si se encontraron datos para el país "Albania"
         if (datosAlbania.length === 0) {
-            res.status(404).json({ error: "No se encontraron datos para el país Albania" });
+            res.sendStatus(404, "Not Found");
         } else {
             res.status(200).json(datosAlbania);
         }
@@ -52,47 +52,58 @@ function api_VEG(app) {
     //POST para crear un nuevo dato
     app.post(API_BASE + "/youtube-trends", (req, res) => {
         const nuevoDato = req.body;
-        
+
         // Verificar si el campo 'id' está presente en el objeto recibido
         if (!nuevoDato.hasOwnProperty('id')) {
-            res.status(400).json({ error: "Falta el campo 'id' en el nuevo dato." });
+            res.sendStatus(400, "Bad Request");
             return;
         }
-        
+
         // Verificar si el ID ya existe en la base de datos
         const idExistente = data_VEG.some(item => item.id === nuevoDato.id);
         if (idExistente) {
-            res.status(409).json({ error: "Conflicto. El ID ya existe en la base de datos." });
+            res.sendStatus(409, "Conflict");
         } else {
             data_VEG.push(nuevoDato);
-            res.status(201).json({ success: "Nuevo dato creado exitosamente." });
+            res.sendStatus(201, "Created");
         }
     });
 
     // POST No permitido en un recurso
     app.post(API_BASE + "/youtube-trends/channel_title", (req, res) => {
-        res.status(405).json({ error: "Método no permitido" });
+        res.sendStatus(405, "Method Not Allowed");
     });
 
 
 
     //PUT para actualizar un dato existente
     app.put(API_BASE + "/youtube-trends/:id", (req, res) => {
-        const id = req.params.id;
-        const nuevoDato = req.body;
-        const index = data_VEG.findIndex(item => item.id === parseInt(id));
-        if (index !== -1) {
-            // Actualizar solo las propiedades proporcionadas en el cuerpo de la solicitud
-            Object.assign(data_VEG[index], nuevoDato);
-            res.status(200).json({ success: "Dato actualizado exitosamente." });
+        const idFromUrl = req.params.id;
+        const newDato = req.body;
+    
+        // Convertir idFromUrl a cadena si es necesario para asegurar la comparación correcta
+        const idFromUrlString = idFromUrl.toString();
+    
+        // Verificar si las IDs coinciden
+        if (idFromUrlString !== newDato.id.toString()) {
+            // Si las IDs son diferentes, responder con el error 400
+            res.sendStatus(400, "Bad Request");
         } else {
-            res.status(404).json({ error: "No se encontró el dato para actualizar." });
+            // Si las IDs coinciden, proceder con la actualización
+            const index = data_VEG.findIndex(item => item.id === parseInt(idFromUrl));
+            if (index !== -1) {
+                // Actualizar solo las propiedades proporcionadas en el cuerpo de la solicitud
+                Object.assign(data_VEG[index], newDato);
+                res.sendStatus(200, "OK");
+            } else {
+                res.sendStatus(404, "Not Found");
+            }
         }
     });
 
     //PUT No permitido en un recurso
     app.put(API_BASE + "/youtube-trends", (req, res) => {
-        res.status(405).json({ error: "Método no permitido" });
+        res.sendStatus(405, "Method Not Allowed");
     });
 
 
@@ -102,9 +113,9 @@ function api_VEG(app) {
         const index = data_VEG.findIndex(item => item.id.toString() === id); // Convertir el ID del objeto a cadena para comparar
         if (index !== -1) {
             data_VEG.splice(index, 1);
-            res.status(200).json({ success: "Dato eliminado exitosamente." });
+            res.sendStatus(200, "OK");
         } else {
-            res.status(404).json({ error: "No se encontró el dato para eliminar." });
+            res.sendStatus(404, "Not Found");
         }
     });
 
@@ -113,7 +124,7 @@ function api_VEG(app) {
         while (data_VEG.length > 0) {
             data_VEG.pop();
         }
-        res.status(200).json({ success: "Todos los recursos han sido eliminados." });
+        res.sendStatus(200, "OK");
     });
 
 
