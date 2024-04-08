b1f23f350f21f79fbc4d8263bedd0f850ccf1933
Víctor Escalera García <vicescgar@us.es>
2024-04-08 14:27:55 +0200

    d02 final

--- front/src/routes/movies-dataset/+page.svelte
+++ front/src/routes/movies-dataset/+page.svelte
@@ -1,16 +1,30 @@
 <script>
     import {onMount} from "svelte";
     import { dev } from "$app/environment";
-    import { Button, ListGroup, ListGroupItem, NavLink, Container, Row, Col, Alert } from '@sveltestrap/sveltestrap';
+    import { page } from '$app/stores';
+    import { Button, ListGroup, ListGroupItem, NavLink, Container, Row, Col, Alert, Pagination, PaginationItem, PaginationLink, Input } from '@sveltestrap/sveltestrap';
+
+
 
     let API = "/api/v1/movies-dataset";
 
     if(dev)
         API = "http://localhost:10002"+API
 
-    let movies = []
-    let errorMsg = ""
-    let successMsg = ""
+    
+
+    // Variables necesarias 
+    let movies = []                         // Almacenar las peliculas a mostrar
+    let errorMsg = ""; let successMsg = ""  // Almacenar los mensajes de error o exito de la pagina
+
+    //if (!paged===true)
+    /*let offset= $page.url.searchParams.get('offset'); */let limit = 10;   // Almacenar los valores necesarios para paginar
+    // if(!$page.url.searchParams.get('offset'))
+    let offset=0;
+    let camposPelicula = []
+    // let objBusqueda = {}
+    let cantidadPeliculas = 0;               // Saber cuantas peliculas hay, mas de 10 habra que paginar
+    let listaPags = []
 
     onMount(() => {
         getMovies();
@@ -22,9 +36,9 @@
             let response = await    fetch(API+"/loadInitialData", {
                                         method: "GET"
                                     });
-            console.log(`Exito cargando peliculas.`);
-            response.type
+            
             if (response.status == 201){
+                console.log(`Exito cargando peliculas.`);
                 getMovies(); 
                 successMsg = "Datos iniciales cargados con exito."
             } else { 
@@ -36,21 +50,121 @@
     }
 
     async function getMovies() {
+        listaPags = []
         successMsg, errorMsg = "", "";
+        let parametros = "";
+        let data = []
+        // Almacenar los valores necesarios para paginar
+        if($page.url.searchParams.get('offset'))
+            offset=$page.url.searchParams.get('offset');
+        // Comprobar si hay que paginar
+        if ($page.url.searchParams.get('offset') || !document.getElementById('campoBusqueda')) {
+            // Si hay que paginar, construimos la query que vamos añadir a la url
+            parametros = `?offset=${offset}&limit=${limit}`;
+        // En caso contrario, es busqueda por campo, si es que esta esa query en la url
+        } else if(document.getElementById('valorBusqueda').value) {
+            // Almacenamos los parametros de busqueda que se han introducido en el filtro de busqueda
+            let campo = document.getElementById('campoBusqueda').value;
+            let valor = document.getElementById('valorBusqueda').value;
+            
+            //window.location.href = `?${campo}=${valor}`
+            // En caso que se pulse el boton buscar sin indicar el valor se indicará al usuario
+            if(valor === "") {
+                errorMsg = "No has introducido ningun valor a buscar"
+            } else {
+                // Si todo va bien, construimos la query que vamos a añadir a la url
+                parametros = `?${campo}=${valor}`;
+            }
+        }
+        
         try {
-            let response = await    fetch(API, {
+            // Contar cuantas paginas hay en el front
+            let cantidad = await    fetch(API, {
                                         method: "GET"
                                     });
-            let data = await response.json();
-            movies = data;
-            console.log(movies);
-            
+            let cant = await cantidad.json();
+            cantidadPeliculas = cant.length;
+            // Actualizamos listaPags, añadiendo un valor de 1 hasta la cantidad de paginas que necesitamos
+            for(let i = 1; i <= Math.ceil(cantidadPeliculas / 10); i++) {listaPags.push(i)};
+            // Esto nos sireve para hacer la paginacion por paginas
+
+            // Obtener las peliculas a mostrar por cada pagina
+            let response = await    fetch(API+parametros, { // En caso de haber querys se añadiran a la busqueda en la API
+                                        method: "GET"
+                                    });
+            data = await response.json();
+            console.log(data)
+            console.log(data[0])
+            // Cabe la posibilidad que al hacer una busqueda, dicha busqueda devuelva unn array vacio de datos
+            if (data[0] instanceof Object && cantidadPeliculas != 0) {
+                // Almacenamos las peliculas que vamos a mostrar
+                movies = data;
+                // Almacenamos las claves para hacer el select de la 
+                if (movies[0] instanceof Object) {
+                    console.log("Por lo que sea si que da true"); 
+                    camposPelicula = Object.keys(movies[0]);
+                }
+                else
+                    console.log("Si esta vacio no es true")
+                console.log(movies);
+            }else if(cantidadPeliculas != 0) {
+                // Si los datos obtenidos estan vacios informa al usuario
+                errorMsg = "No hay registrados datos relacionados con esta búsqueda";
+            }
+            // En caso de hacer paginación, recargamos la pagina, sino, lo tendría que hacer el usuario
+            if($page.url.searchParams.get('offset') || document.getElementById('valorBusqueda'))
+                window.location.reload;
             if (response.status != 200) {
                 errorMsg = "No se ha encontrado la colección."
             }
         } catch (error) {
-            errorMsg = error;
+            errorMsg = error+"L118";
         }
+    }
+
+    function bucarCampos() {
+        // Para tener un select con todos los campos 
+        camposPelicula = Object.keys(movies[0])
+        // Almacenamos los parametros de busqueda que se han introducido en el filtro de busqueda
+        let campo = document.getElementById('campoBusqueda').value;
+        let valor = document.getElementById('valorBusqueda').value;
+            
+        //window.location.href = `?${campo}=${valor}`
+        // En caso que se pulse el boton buscar sin indicar el valor se indicará al usuario
+        if(valor === "") {
+            errorMsg = "No has introducido ningun valor a buscar"
+        } else {
+            // Si todo va bien, construimos la query que vamos a añadir a la url
+            // parametros = `?${campo}=${valor}`;
+            window.location.href = `?${campo}=${valor}`;
+        }
+    }
+
+    function anteriorPagina(ofs) {
+        if(ofs > 0) {
+            window.location.href = `?offset=${ofs-10}&limit=10`
+            getMovies();
+        } else {
+            errorMsg = "Esta es la primera página, no puedes retroceder mas"
+        }   
+    }
+
+    function siguientePagina(ofs) {
+        if (parseInt(ofs) >= Math.trunc(cantidadPeliculas / 10)*10 || cantidadPeliculas === 10) {
+            errorMsg = "Esta es la última página, no puedes avanzar mas"
+        } else {
+            ofs = parseInt(ofs) + 10
+            window.location.href = `?offset=${ofs}&limit=10`
+        }
+    }
+
+    function paginar(pagina) {
+        let ofs = $page.url.searchParams.get('offset');
+        // Si estoy en la pagina 1, tengo que usar offset=0, pagina(1)-1*ofs(0)=0
+        // Si estoy en la pagina 2, tengo que usar offset=10, pagina(2)-1(1)*ofs(10)=10
+        ofs = (pagina-1)*limit
+        console.log(`Cuanto coño vale ${ofs}`)
+        window.location.href = `/movies-dataset?offset=${ofs}&llimit=10`
     }
 
     async function deleteMovie(title) {
@@ -62,7 +176,9 @@
                                         method: "DELETE"
                                     });
             if (response.status == 200){
-                getMovies();
+                await getMovies();
+                if (cantidadPeliculas === 10)
+                    window.location.href = "/movies-dataset"
                 successMsg = `La pelicula ${title} se ha borrado correctamente.`
             }
             else
@@ -81,6 +197,7 @@
                                     });
             console.log(`Deleted`);
             if (response.status == 200){
+                window.location.href = "/movies-dataset"
                 getMovies();
                 successMsg = "Colección borrada con exito."
             } else
@@ -89,6 +206,20 @@
             errorMsg = error;
         }
     }
+    // Codigo para futuro
+    // function firstOlast(pagina) {
+    //     console.log("Estamos en la "+pagina)
+    //     if (pagina === "Primera")
+    //         window.location.href = "/movies-dataset?offset=0&llimit=10"
+    //     else if (pagina == "Ultima")
+    //         window.location.href = `/movies-dataset?offset=${Math.ceil(cantidadPeliculas / 10)*10}&llimit=10`
+    // }
+    //             <PaginationItem>
+    //                 <PaginationLink first on:click={firstOlast}/>
+    //             </PaginationItem>
+    //             <PaginationItem>
+    //                 <PaginationLink last on:click={firstOlast}/>
+    //             </PaginationItem>
 </script>
 
 <Container>
@@ -112,26 +243,46 @@
         {#if movies.length == 0}
             <p>La lista está vacía</p>
             <p>Para insertar datos pulsa este botón -> <Button size="md" outline color="primary" on:click={loadInitialData}>Rellenar</Button></p>
+        {:else}
+            <Col>
+                Búsqueda por campos 
+            </Col>
+            <Col xs=2>
+                <Input bsSize=sm type="select" id="campoBusqueda">
+                    {#each camposPelicula as campo}
+                      <option>{campo}</option>
+                    {/each}
+                </Input>
+            </Col>
+            <Col xs=2>
+                <Input bsSize=sm id="valorBusqueda"></Input>
+            </Col>
+            <Col xs=2>
+                <Button class="buscarCampo" on:click={getMovies}>Buscar</Button>
+            </Col>
         {/if}
     </Row>
     <div>
-        <ListGroup>
+        <ListGroup class="movieList">
             {#each movies as movie}
             <ListGroupItem>
                 <Row>
                     <Col xs=10>
-                        <NavLink active href="movies-dataset/{movie.original_title}">{movie.original_title}</NavLink>
+                        <NavLink active href="movies-dataset/{movie.original_title}">{movie.index+1}. {movie.original_title}</NavLink>
                         
                         <strong>Director:</strong> {movie.director}, <strong>Estreno:</strong> {movie.release_date}
                     </Col>
                     
-                    <Col xs="2" class="d-flex justify-content-center"><Button size="sm" color="danger" on:click={deleteMovie(movie.original_title)}>Borrar</Button></Col> 
+                    <Col sm="12" md={{ size: 2, offset: 'auto' }}>
+                        <div class="d-flex justify-content-center align-items-center">
+                            <Button style="margin-top: auto;" size="sm" color="danger" on:click={deleteMovie(movie.original_title)}>Borrar</Button>
+                        </div>
+                    </Col> 
                 </Row>
             </ListGroupItem>
             {/each}
         </ListGroup>
     </div>
-    
     {#if !movies.length == 0}
     <Row>
         <Col xs="auto">Añade una nueva película <Button href="movies-dataset/añadirPelicula" size="sm" color="warning">Añadir</Button></Col>
@@ -139,5 +290,24 @@
     <Row>
         <Col xs="auto">Eliminar la coleccion <Button outline size="sm" color="danger" on:click={deleteColection}>Borrar Todo</Button></Col>
     </Row>
+        {#if listaPags.length != 0}
+        <Row>
+            <Col sm="12" md={{ size: 4, offset: 4 }} class="d-flex justify-content-center">
+                <Pagination size="md" ariaLabel="Paginacion del front-end">
+                    <PaginationItem>
+                        <PaginationLink class="AnteriorPag" previous on:click={anteriorPagina(offset)}/>
+                    </PaginationItem>
+                    {#each listaPags as pagina}
+                    <PaginationItem>
+                        <PaginationLink class="BotonPagina{pagina}" on:click={paginar(pagina)}>{pagina}</PaginationLink>
+                    </PaginationItem>
+                    {/each}
+                    <PaginationItem>
+                        <PaginationLink class="SiguientePag" next on:click={siguientePagina(offset)}/>
+                    </PaginationItem>
+                </Pagination>
+            </Col>
+        </Row>
+        {/if}
     {/if}
 </Container>
--- tests/e2e/events.spec.cjs
+++ tests/e2e/events.spec.cjs
@@ -1,79 +1,78 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-//---------------Verifica si la página tiene un título esperado----------------
+//---------------Verifica si la página tiene un título esperado---------------
 test('tiene título', async ({ page }) => {
-  await page.goto('http:/localhost:10002');
+  await page.goto('http://localhost:10002/ufc-events-data');
 
   // Espera que el título "contenga" una subcadena.
   await expect(page).toHaveTitle(/APIs Grupo 14/);
 });
 
+/*
 //---------------Verifica la funcionalidad de carga de datos iniciales---------------
 test('cargar datos iniciales', async ({ page }) => {
-  await page.goto('http:/localhost:10002/youtube-trends');
+  await page.goto('http://localhost:5173/movies-dataset');
 
   // Haz clic en el botón "Cargar Datos Iniciales"
-  await page.click('button:text("Cargar Datos Iniciales")');
+  await page.click('button:text("Rellenar")');
 
   // Espera a que aparezca el mensaje de éxito después de cargar los datos iniciales
   await page.waitForSelector('.alert', { timeout: 5000 }); // Espera a que aparezca cualquier elemento con la clase .alert
   const successMsg = await page.textContent('.alert');
 
   // Verifica si el mensaje de éxito contiene el texto esperado
-  expect(successMsg).toContain('Datos iniciales cargados exitosamente');
+  expect(successMsg).toContain('Exito Datos iniciales cargados con exito.');
 });
 
 //---------------Verifica la funcionalidad de búsqueda---------------
 test('funcionalidad de búsqueda', async ({ page }) => {
-  await page.goto('http:/localhost:10002/youtube-trends');
+  await page.goto('http://localhost:5173/movies-dataset');
 
   // Ingresa un término de búsqueda y espera que la lista se actualice
-  await page.fill('input[type="text"]', 'Prueba');
-  await page.click('button');
+  await page.fill('input[id="campoBusqueda"]', 'index');
+  await page.fill('input[id="valorBusqueda"]', '1');
+  await page.click('button:class("buscarCampo")');
 
   // Espera que al menos haya un elemento en la lista de videos después de la búsqueda
-  await page.waitForSelector('.videoItem');
-  const videos = await page.$$('.videoItem');
+  await page.waitForSelector('.movieList');
+  const videos = await page.$$('.movieList');
   expect(videos.length).toBeGreaterThanOrEqual(1);
 });
 
 //---------------Verifica la funcionalidad de paginación---------------
 test('funcionalidad de paginación', async ({ page }) => {
-  await page.goto('http:/localhost:10002/youtube-trends');
+  await page.goto('http://localhost:5173/movies-dataset');
 
   // Espera que los botones de paginación estén presentes
-  await page.waitForSelector('button');
+  await page.waitForSelector('PaginationLink');
 
   // Haz clic en el botón "Página Siguiente"
   await Promise.all([
-    page.click('button:text("Página Siguiente")'),
-    page.waitForSelector('.videoItem', { timeout: 30000 }), // Espera a que la lista de videos se actualice con la página siguiente
+    page.click('PaginationLink:class("SiguientePag")'),
+    page.waitForSelector('.movieList', { timeout: 30000 }), // Espera a que la lista de videos se actualice con la página siguiente
   ]);
 
   // Haz clic en el botón "Página Anterior"
-  await page.click('button:text("Página Anterior")');
+  await page.click('PaginationLink:class("AnteriorPag")');
 
   // Espera a que la lista de videos se actualice con la página anterior
-  await page.waitForSelector('.videoItem', { timeout: 30000 });
+  await page.waitForSelector('.movieList', { timeout: 30000 });
 });
 
 //---------------Verifica la funcionalidad del botón "Borrar todo"---------------
 test('funcionalidad "Borrar todo"', async ({ page }) => {
-  await page.goto('http:/localhost:10002/youtube-trends');
+  await page.goto('http://localhost:5173/movies-dataset');
 
   // Haz clic en el botón "Borrar todo"
-  await page.click('button:text("Borrar todo")');
+  await page.click('button:text("Borrar Todo")');
 
   // Espera a que se procese la eliminación de todos los datos
   await page.waitForTimeout(2000);
 
   // Verifica que la lista de videos esté vacía después de borrar todo
-  const videos = await page.$$('.videoItem');
+  const videos = await page.$$('.movieList');
   expect(videos.length).toBe(0);
 });
 
-
-
-
-
+*/
