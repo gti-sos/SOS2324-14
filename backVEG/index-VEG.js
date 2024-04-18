d048e25bf5ad521f2cbab55364cd7ec30a7e0e45
Víctor Escalera García <vicescgar@us.es>
2024-04-18 11:18:38 +0200

    cambio formato fecha

--- backVEG/index-VEG.js
+++ backVEG/index-VEG.js
@@ -1,14 +1,14 @@
 const data_VEG = [
-    { ranking: 0, country: 'Albania', title: 'DON XHONI - KATILE', published_at: '2022-07-07T22:00:12Z', channel_title: 'DON XHONI', category_id: 24, trending_date: '22.11.07', view_count: 5130167, comment_count: 3641 },
-    { ranking: 1, country: 'Albania', title: 'Dhurata Dora x Elvana Gjata - GAJDE (Official Video)', published_at: '2022-06-30T22:00:11Z', channel_title: 'Dhurata Dora', category_id: 10, trending_date: '22.11.07', view_count: 8886648, comment_count: 4414 },
-    { ranking: 2, country: 'Albania', title: 'KIDA x BUTRINT IMERI - AM/PM', published_at: '2022-07-02T22:00:11Z', channel_title: 'KIDA', category_id: 10, trending_date: '22.11.07', view_count: 7230818, comment_count: 7518 },
-    { ranking: 3, country: 'Albania', title: 'Dafina Zeqiri - Malli', published_at: '2022-07-07T22:00:12Z', channel_title: 'Dafina Zeqiri', category_id: 10, trending_date: '22.11.07', view_count: 1406078, comment_count: 827 },
-    { ranking: 4, country: 'Albania', title: 'Tayna x Azet - Tequila', published_at: '2022-06-30T18:00:26Z', channel_title: 'Friends Entertainment', category_id: 10, trending_date: '22.11.07', view_count: 6397676, comment_count: 6063 },
-    { ranking: 5, country: 'Albania', title: 'GJIKO x FIFI - DJEGI KREJT', published_at: '2022-07-07T17:45:11Z', channel_title: 'REDBOX Entertainment', category_id: 10, trending_date: '22.11.07', view_count: 1695364, comment_count: 1961 },
-    { ranking: 6, country: 'Albania', title: 'Ardian Bujupi x Ledri Vula - GELATO (prod. by Bled)', published_at: '2022-06-30T22:00:11Z', channel_title: 'OfficialArdianBujupi', category_id: 10, trending_date: '22.11.07', view_count: 3316092, comment_count: 1391 },
-    { ranking: 7, country: 'Albania', title: 'Capital T - Dukat (prod. Panda Music)', published_at: '2022-06-29T16:00:03Z', channel_title: 'Capital T', category_id: 10, trending_date: '22.11.07', view_count: 7110466, comment_count: 1230 },
-    { ranking: 8, country: 'Albania', title: 'MatoLale x Jongmen KOKAINA', published_at: '2022-07-05T16:30:27Z', channel_title: 'MATOLALE', category_id: 10, trending_date: '22.11.07', view_count: 1157204, comment_count: 929 },
-    { ranking: 9, country: "España", title: "España campeona del mundo", published_at: "2010-07-011T22:00:12Z", channel_title: "SEFutbol", category_id: 2, trending_date: "10.07.11", view_count: 514000, comment_count: 3651 }
+    { ranking: 0, country: 'Albania', title: 'DON XHONI - KATILE', published_at: '2022-07-07', channel_title: 'DON XHONI', category_id: 24, trending_date: '22.11.07', view_count: 5130167, comment_count: 3641 },
+    { ranking: 1, country: 'Albania', title: 'Dhurata Dora x Elvana Gjata - GAJDE (Official Video)', published_at: '2022-06-30', channel_title: 'Dhurata Dora', category_id: 10, trending_date: '22.11.07', view_count: 8886648, comment_count: 4414 },
+    { ranking: 2, country: 'Albania', title: 'KIDA x BUTRINT IMERI - AM/PM', published_at: '2022-07-02', channel_title: 'KIDA', category_id: 10, trending_date: '22.11.07', view_count: 7230818, comment_count: 7518 },
+    { ranking: 3, country: 'Albania', title: 'Dafina Zeqiri - Malli', published_at: '2022-07-07', channel_title: 'Dafina Zeqiri', category_id: 10, trending_date: '22.11.07', view_count: 1406078, comment_count: 827 },
+    { ranking: 4, country: 'Albania', title: 'Tayna x Azet - Tequila', published_at: '2022-06-30', channel_title: 'Friends Entertainment', category_id: 10, trending_date: '22.11.07', view_count: 6397676, comment_count: 6063 },
+    { ranking: 5, country: 'Albania', title: 'GJIKO x FIFI - DJEGI KREJT', published_at: '2022-07-07', channel_title: 'REDBOX Entertainment', category_id: 10, trending_date: '22.11.07', view_count: 1695364, comment_count: 1961 },
+    { ranking: 6, country: 'Albania', title: 'Ardian Bujupi x Ledri Vula - GELATO (prod. by Bled)', published_at: '2022-06-30', channel_title: 'OfficialArdianBujupi', category_id: 10, trending_date: '22.11.07', view_count: 3316092, comment_count: 1391 },
+    { ranking: 7, country: 'Albania', title: 'Capital T - Dukat (prod. Panda Music)', published_at: '2022-06-29', channel_title: 'Capital T', category_id: 10, trending_date: '22.11.07', view_count: 7110466, comment_count: 1230 },
+    { ranking: 8, country: 'Albania', title: 'MatoLale x Jongmen KOKAINA', published_at: '2022-07-05', channel_title: 'MATOLALE', category_id: 10, trending_date: '22.11.07', view_count: 1157204, comment_count: 929 },
+    { ranking: 9, country: "España", title: "España campeona del mundo", published_at: "2010-07-11", channel_title: "SEFutbol", category_id: 2, trending_date: "10.07.11", view_count: 514000, comment_count: 3651 }
 ];
 
 // Función para calcular la media de view_count para un país específico
