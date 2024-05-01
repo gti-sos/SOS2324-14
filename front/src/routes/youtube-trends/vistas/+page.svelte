f494a238ff6d4d01efdf1f5819cc982eb8b79f5f
Víctor Escalera García <vicescgar@us.es>
2024-05-01 18:24:54 +0200

    graficas apexcharts (D03B a)

--- front/src/routes/youtube-trends/vistas/+page.svelte
+++ front/src/routes/youtube-trends/vistas/+page.svelte
@@ -1,167 +1,161 @@
-<svelte:head>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <title>YouTubeTrends</title>
-</svelte:head>
-
 <script>
-    import { onMount } from "svelte";
-    import { dev } from '$app/environment';
-    import { Container } from '@sveltestrap/sveltestrap';
+	import { onMount } from 'svelte';
+	import { dev } from '$app/environment';
+	import { Container } from '@sveltestrap/sveltestrap';
 
-    let API = '/api/v1/youtube-trends';
+	let API = '/api/v1/youtube-trends';
 
 	if (dev) API = 'http://localhost:10002' + API;
 
-    async function getData(){
-    try{
-        const res = await fetch(API);
-        const data = await res.json();
-        console.log(`Dato recibido`);
-        if(data.length > 0) {
-            fillBarChart(data);
-            createScatterPlot(data);
-        } else {
-            console.log('No hay datos disponibles');
-            showNoDataMessage();
-        }
-    } catch(error){
-        console.log(`Error: ${error}`)
-    }
-}
+	async function getData() {
+		try {
+			const res = await fetch(API);
+			const data = await res.json();
+			console.log(`Dato recibido`);
+			if (data.length > 0) {
+				fillBarChart(data);
+				createScatterPlot(data);
+			} else {
+				console.log('No hay datos disponibles');
+				showNoDataMessage();
+			}
+		} catch (error) {
+			console.log(`Error: ${error}`);
+		}
+	}
 
-    function fillBarChart(data){
-        const countriesData = processDataForBarChart(data);
-        
-        const chart = Highcharts.chart('bar-container', {
-            chart: {
-                type: 'column'
-            },
-            title: {
-                text: 'Visitas por País'
-            },
-            xAxis: {
-                categories: countriesData.map(item => item.country)
-            },
-            yAxis: {
-                title: {
-                    text: 'Visitas'
-                }
-            },
-            series: [{
-                name: 'Visitas',
-                data: countriesData.map(item => item.view_count)
-            }]
-        });
-    }
+	function fillBarChart(data) {
+		const countriesData = processDataForBarChart(data);
 
-    function processDataForBarChart(data){
-        const countriesData = {};
-        data.forEach(item => {
-            if(countriesData[item.country]){
-                countriesData[item.country] += item.view_count;
-            } else {
-                countriesData[item.country] = item.view_count;
-            }
-        });
-        return Object.entries(countriesData).map(([country, view_count]) => ({ country, view_count }));
-    }
+		const options = {
+			chart: {
+				type: 'bar'
+			},
+			series: [
+				{
+					name: 'Visitas',
+					data: countriesData.map((item) => item.view_count)
+				}
+			],
+			xaxis: {
+				categories: countriesData.map((item) => item.country)
+			},
+			yaxis: {
+				title: {
+					text: 'Visitas'
+				}
+			},
+			title: {
+				text: 'Visitas por País'
+			}
+		};
 
-    function createScatterPlot(data){
-        const chartData = processDataForScatterPlot(data);
+		const chart = new ApexCharts(document.querySelector('#bar-container'), options);
+		chart.render();
+	}
 
-        Highcharts.chart('scatter-container', {
-            chart: {
-                type: 'scatter',
-                zoomType: 'xy'
-            },
-            title: {
-                text: 'Relación entre comentarios y visualizaciones'
-            },
-            xAxis: {
-                title: {
-                    enabled: true,
-                    text: 'Visualizaciones'
-                },
-                startOnTick: true,
-                endOnTick: true,
-                showLastLabel: true
-            },
-            yAxis: {
-                title: {
-                    text: 'Comentarios'
-                }
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'left',
-                verticalAlign: 'top',
-                x: 100,
-                y: 70,
-                floating: true,
-                backgroundColor: Highcharts.defaultOptions.chart.backgroundColor,
-                borderWidth: 1
-            },
-            plotOptions: {
-                scatter: {
-                    marker: {
-                        radius: 5,
-                        states: {
-                            hover: {
-                                enabled: true,
-                                lineColor: 'rgb(100,100,100)'
-                            }
-                        }
-                    },
-                    states: {
-                        hover: {
-                            marker: {
-                                enabled: false
-                            }
-                        }
-                    },
-                    tooltip: {
-                        headerFormat: '<b>{series.name}</b><br>',
-                        pointFormat: '{point.title}: {point.y} comentarios, {point.x} visualizaciones'
-                    }
-                }
-            },
-            series: [{
-                name: 'Videos',
-                color: 'rgba(223, 83, 83, .5)',
-                data: chartData
-            }]
-        });
-    }
+	function processDataForBarChart(data) {
+		const countriesData = {};
+		data.forEach((item) => {
+			if (countriesData[item.country]) {
+				countriesData[item.country] += item.view_count;
+			} else {
+				countriesData[item.country] = item.view_count;
+			}
+		});
+		return Object.entries(countriesData).map(([country, view_count]) => ({ country, view_count }));
+	}
 
-    function processDataForScatterPlot(data){
-        return data.map(video => ({
-            x: video.view_count,
-            y: video.comment_count,
-            title: video.title
-        }));
-    }
+	function createScatterPlot(data) {
+		const chartData = processDataForScatterPlot(data);
 
-    function showNoDataMessage() {
-        console.log('Showing no data message');
-        const container = document.getElementById('chart-container');
-        container.innerHTML = '<p style="margin-left: 20px;">No hay datos para mostrar gráficas</p>';
-    }
+		const options = {
+			chart: {
+				type: 'scatter',
+				zoom: {
+					enabled: true,
+					type: 'xy'
+				}
+			},
+			series: [
+				{
+					name: 'Videos',
+					data: chartData
+				}
+			],
+			xaxis: {
+				title: {
+					text: 'Visualizaciones'
+				}
+			},
+			yaxis: {
+				title: {
+					text: 'Comentarios'
+				}
+			},
+			title: {
+				text: 'Relación entre comentarios y visualizaciones'
+			},
+			markers: {
+				size: 5
+			},
+			tooltip: {
+				custom: function ({ dataPointIndex }) {
+					const videoTitle = data[dataPointIndex].title;
+					const visualizaciones = data[dataPointIndex].view_count;
+					const comentarios = data[dataPointIndex].comment_count;
+					const ranking = data[dataPointIndex].ranking;
+					const country = data[dataPointIndex].country;
+					const published_at = data[dataPointIndex].published_at;
+					const channel_title = data[dataPointIndex].channel_title;
+					const category_id = data[dataPointIndex].category_id;
+					const trending_date = data[dataPointIndex].trending_date;
 
-    onMount(()=>{
-        getData();
-    })
+					return `<div class="apexcharts-tooltip-title">${videoTitle}</div>
+                <div>Comentarios: ${comentarios}</div>
+                <div>Visualizaciones: ${visualizaciones}</div>
+                <div>Ranking: ${ranking}</div>
+                <div>Country: ${country}</div>
+                <div>Published_at: ${published_at}</div>
+                <div>Channel_title: ${channel_title}</div>
+                <div>Category_id: ${category_id}</div>
+                <div>Trending_date: ${trending_date}</div>`;
+				}
+			}
+		};
 
+		const chart = new ApexCharts(document.querySelector('#scatter-container'), options);
+		chart.render();
+	}
+
+	function processDataForScatterPlot(data) {
+		return data.map((video) => ({
+			x: video.view_count,
+			y: video.comment_count
+		}));
+	}
+
+	function showNoDataMessage() {
+		console.log('Showing no data message');
+		const container = document.getElementById('chart-container');
+		container.innerHTML = '<p style="margin-left: 20px;">No hay datos para mostrar gráficas</p>';
+	}
+
+	onMount(() => {
+		getData();
+	});
 </script>
 
+<svelte:head>
+	<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+	<title>YouTubeTrends</title>
+</svelte:head>
+
 <Container>
-    <h1><strong>Gráficas</strong></h1>
+	<h1><strong>Gráficas</strong></h1>
 </Container>
 
 <Container id="chart-container">
-    <div id="bar-container" style="width:100%; height:400px;"></div>
-    <div id="scatter-container" style="width:100%; height:400px;"></div>
+	<div id="bar-container" style="width: 80%; height: 300px;"></div>
+	<div id="scatter-container" style="width: 80%; height: 300px;"></div>
 </Container>
-
-
-
-
