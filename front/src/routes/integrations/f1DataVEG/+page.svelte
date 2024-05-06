4b7f05c3e6ba8a92514519f4e175b6d0e120563a
Víctor Escalera García <vicescgar@us.es>
2024-05-06 15:33:40 +0200

    Uso/Integración API Formula 1(HAY MUNDIAL)

---
+++ front/src/routes/integrations/f1DataVEG/+page.svelte
@@ -0,0 +1,80 @@
+<script>
+	import axios from 'axios';
+	import { onMount } from 'svelte';
+	import { Container, ListGroup, ListGroupItem } from '@sveltestrap/sveltestrap';
+
+	let pilotData = [];
+
+	const options = {
+		method: 'GET',
+		url: 'https://f1-live-motorsport-data.p.rapidapi.com/drivers/standings/2024',
+		headers: {
+			'X-RapidAPI-Key': 'c4dcccf12bmshb28d319bf18afe1p17ebd3jsn3d5ff8dfec68',
+			'X-RapidAPI-Host': 'f1-live-motorsport-data.p.rapidapi.com'
+		}
+	};
+
+	onMount(async () => {
+		try {
+			const response = await axios.request(options);
+			if (response.data.results && response.data.results.length > 0) {
+				pilotData = response.data.results.map((driver) => ({
+					name: driver.driver_name,
+					points: parseInt(driver.points)
+				}));
+				renderChart();
+			} else {
+				console.error('La respuesta de la API no contiene datos válidos.');
+			}
+		} catch (error) {
+			console.error('Error al obtener los datos de los pilotos:', error);
+		}
+	});
+
+	function renderChart() {
+		// Verificar si el elemento #chart está presente en el DOM
+		const chartElement = document.querySelector('#chart');
+		if (!chartElement) {
+			console.error('Elemento #chart no encontrado en el DOM.');
+			return;
+		}
+
+		var options = {
+			chart: {
+				type: 'bar',
+				height: 350,
+				id: 'chart'
+			},
+			series: [
+				{
+					name: 'Puntos',
+					data: pilotData.map((driver) => driver.points)
+				}
+			],
+			xaxis: {
+				type: 'category',
+				categories: pilotData.map((driver) => driver.name),
+				labels: {
+					rotate: -45
+				}
+			}
+		};
+
+		var chart = new ApexCharts(chartElement, options);
+		chart.render();
+	}
+</script>
+
+<svelte:head>
+	<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+</svelte:head>
+
+<Container>
+    <h1>Integración API Externa 2(F1 Live Motorsport Data)</h1>
+    <h5>Clasificación mundial de pilotos por temporada (2024)</h5>
+	<div id="chart"></div>
+
+	{#if pilotData.length === 0}
+		<p>Cargando datos...</p>
+	{/if}
+</Container>
---
+++ front/src/routes/integrations/youtubeDataVEG/+page.svelte
@@ -0,0 +1,108 @@
+<script>
+    import axios from 'axios';
+    import { onMount } from 'svelte';
+    import { Container, ListGroup, ListGroupItem } from '@sveltestrap/sveltestrap';
+
+    // Definir las opciones para la primera llamada a la API
+    const options1 = {
+        method: 'GET',
+        url: 'https://youtube-data8.p.rapidapi.com/channel/details/',
+        params: {
+            id: 'UC-lHJZR3Gqxm24_Vd_AJ5Yw'
+        },
+        headers: {
+            'X-RapidAPI-Key': 'c4dcccf12bmshb28d319bf18afe1p17ebd3jsn3d5ff8dfec68',
+            'X-RapidAPI-Host': 'youtube-data8.p.rapidapi.com'
+        }
+    };
+
+    // Variable que guarda los detalles del canal
+    let channelDetails = null;
+
+    // Función para obtener los datos del primer endpoint
+    const fetchChannelDetails = async () => {
+        try {
+            const response = await axios.request(options1);
+            channelDetails = response.data;
+        } catch (error) {
+            console.error('Error al obtener los detalles del canal:', error);
+        }
+    };
+
+    // Definir las opciones para la segunda llamada a la API
+    const options2 = {
+        method: 'GET',
+        url: 'https://youtube-data8.p.rapidapi.com/playlist/videos/',
+        params: {
+            id: 'PLcirGkCPmbmFeQ1sm4wFciF03D_EroIfr',
+            hl: 'en',
+            gl: 'US'
+        },
+        headers: {
+            'X-RapidAPI-Key': 'c4dcccf12bmshb28d319bf18afe1p17ebd3jsn3d5ff8dfec68',
+            'X-RapidAPI-Host': 'youtube-data8.p.rapidapi.com'
+        }
+    };
+
+    // Variable que guarda los datos de la segunda llamada a la API
+    let playlistVideos = null;
+
+    // Función para obtener los datos del segundo endpoint
+    const fetchPlaylistVideos = async () => {
+        try {
+            const response = await axios.request(options2);
+            playlistVideos = response.data;
+        } catch (error) {
+            console.error('Error al obtener los videos de la lista de reproducción:', error);
+        }
+    };
+
+    // Llamar a ambas funciones para obtener los datos cuando el componente se monte
+    onMount(() => {
+        fetchChannelDetails();
+        fetchPlaylistVideos();
+    });
+</script>
+
+<Container>
+    <h1>Uso API Externa 1 (YouTube Data)</h1>
+    <h5>Detalles de un canal</h5>
+    {#if channelDetails}
+        <ListGroup>
+            <ListGroupItem>Título: {channelDetails.title}</ListGroupItem>
+            <ListGroupItem>Nombre de usuario: {channelDetails.username}</ListGroupItem>
+            <ListGroupItem>Suscriptores: {channelDetails.stats.subscribersText}</ListGroupItem>
+            <ListGroupItem>Videos: {channelDetails.stats.videosText}</ListGroupItem>
+            <ListGroupItem>Vistas: {channelDetails.stats.views}</ListGroupItem>
+            <ListGroupItem>Descripción: {channelDetails.description}</ListGroupItem>
+            <ListGroupItem>
+                Enlaces:
+                <ul>
+                    {#each channelDetails.links as link}
+                        <li><a href={link.targetUrl} target="_blank">{link.title}</a></li>
+                    {/each}
+                </ul>
+            </ListGroupItem>
+        </ListGroup>
+    {:else}
+        <p>Cargando detalles del canal...</p>
+    {/if}
+    <hr>
+    <h5>Videos de la Lista de Reproducción</h5>
+
+    {#if playlistVideos}
+        <ListGroup>
+            {#each playlistVideos.contents as video}
+                <ListGroupItem>
+                    <a href={`https://www.youtube.com/watch?v=${video.video.videoId}`} target="_blank">{video.video.title}</a>
+                    <p>Duración: {video.video.lengthSeconds} segundos</p>
+                </ListGroupItem>
+            {/each}
+        </ListGroup>
+    {:else}
+        <p>Cargando videos de la lista de reproducción...</p>
+    {/if}
+</Container>
+
+
+
