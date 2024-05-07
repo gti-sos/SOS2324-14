fbcbeffcde1b28c5b814a7a9dc650a8bb2c1c24a
NicoRedondoo <redondonico9@gmail.com>
2024-05-07 13:33:11 +0200

    commit

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ let app = express();
 
 const PORT = (process.env.PORT || 10002);
 
-app.use(cors());
+app.use(cors({
+    "origin": "*",
+    "methods" : "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus":204
+}));
 
 app.use(bodyParser.json());
 
