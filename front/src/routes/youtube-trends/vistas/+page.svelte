521ba4d3399943ee906a4d146b7f906b1800bcd4
Víctor Escalera García <vicescgar@us.es>
2024-05-13 21:02:04 +0200

    cambio graficas individuales

--- front/src/routes/youtube-trends/vistas/+page.svelte
+++ front/src/routes/youtube-trends/vistas/+page.svelte
@@ -13,8 +13,9 @@
 			const data = await res.json();
 			console.log(`Dato recibido`);
 			if (data.length > 0) {
-				fillBarChart(data);
-				createScatterPlot(data);
+				createBarChartWithHighcharts(data);
+				createScatterPlotWithHighcharts(data);
+				createBarChartWithApexCharts(data); // Nueva función agregada
 			} else {
 				console.log('No hay datos disponibles');
 				showNoDataMessage();
@@ -24,34 +25,36 @@
 		}
 	}
 
-	function fillBarChart(data) {
+	function createBarChartWithHighcharts(data) {
 		const countriesData = processDataForBarChart(data);
 
 		const options = {
 			chart: {
-				type: 'bar'
+				type: 'column'
+			},
+			title: {
+				text: 'Visitas por País (Highcharts)'
+			},
+			xAxis: {
+				categories: countriesData.map((item) => item.country),
+				title: {
+					text: 'País'
+				}
+			},
+			yAxis: {
+				title: {
+					text: 'Visitas'
+				}
 			},
 			series: [
 				{
 					name: 'Visitas',
 					data: countriesData.map((item) => item.view_count)
 				}
-			],
-			xaxis: {
-				categories: countriesData.map((item) => item.country)
-			},
-			yaxis: {
-				title: {
-					text: 'Visitas'
-				}
-			},
-			title: {
-				text: 'Visitas por País'
-			}
+			]
 		};
 
-		const chart = new ApexCharts(document.querySelector('#bar-container'), options);
-		chart.render();
+		Highcharts.chart('highcharts-bar-container', options);
 	}
 
 	function processDataForBarChart(data) {
@@ -66,73 +69,131 @@
 		return Object.entries(countriesData).map(([country, view_count]) => ({ country, view_count }));
 	}
 
-	function createScatterPlot(data) {
+	function createScatterPlotWithHighcharts(data) {
 		const chartData = processDataForScatterPlot(data);
 
 		const options = {
 			chart: {
 				type: 'scatter',
-				zoom: {
-					enabled: true,
-					type: 'xy'
+				zoomType: 'xy'
+			},
+			title: {
+				text: 'Relación entre comentarios y visualizaciones (Highcharts)'
+			},
+			xAxis: {
+				title: {
+					text: 'Visualizaciones'
 				}
+			},
+			yAxis: {
+				title: {
+					text: 'Comentarios'
+				}
+			},
+			tooltip: {
+				headerFormat: '<b>{series.name}</b><br>',
+				pointFormat:
+					'Comentarios: {point.y}, Visualizaciones: {point.x}<br>' +
+					'Title: {point.title}<br>' +
+					'Ranking: {point.ranking}<br>' +
+					'Country: {point.country}<br>' +
+					'Published_at: {point.published_at}<br>' +
+					'Channel_title: {point.channel_title}<br>' +
+					'Category_id: {point.category_id}<br>' +
+					'Trending_date: {point.trending_date}'
 			},
 			series: [
 				{
 					name: 'Videos',
 					data: chartData
 				}
-			],
-			xaxis: {
-				title: {
-					text: 'Visualizaciones'
-				}
-			},
-			yaxis: {
-				title: {
-					text: 'Comentarios'
-				}
-			},
-			title: {
-				text: 'Relación entre comentarios y visualizaciones'
-			},
-			markers: {
-				size: 5
-			},
-			tooltip: {
-				custom: function ({ dataPointIndex }) {
-					const videoTitle = data[dataPointIndex].title;
-					const visualizaciones = data[dataPointIndex].view_count;
-					const comentarios = data[dataPointIndex].comment_count;
-					const ranking = data[dataPointIndex].ranking;
-					const country = data[dataPointIndex].country;
-					const published_at = data[dataPointIndex].published_at;
-					const channel_title = data[dataPointIndex].channel_title;
-					const category_id = data[dataPointIndex].category_id;
-					const trending_date = data[dataPointIndex].trending_date;
-
-					return `<div class="apexcharts-tooltip-title">${videoTitle}</div>
-                <div>Comentarios: ${comentarios}</div>
-                <div>Visualizaciones: ${visualizaciones}</div>
-                <div>Ranking: ${ranking}</div>
-                <div>Country: ${country}</div>
-                <div>Published_at: ${published_at}</div>
-                <div>Channel_title: ${channel_title}</div>
-                <div>Category_id: ${category_id}</div>
-                <div>Trending_date: ${trending_date}</div>`;
-				}
-			}
+			]
 		};
 
-		const chart = new ApexCharts(document.querySelector('#scatter-container'), options);
-		chart.render();
+		Highcharts.chart('highcharts-scatter-container', options);
 	}
 
 	function processDataForScatterPlot(data) {
 		return data.map((video) => ({
 			x: video.view_count,
-			y: video.comment_count
+			y: video.comment_count,
+			title: video.title,
+			ranking: video.ranking,
+			country: video.country,
+			published_at: video.published_at,
+			channel_title: video.channel_title,
+			category_id: video.category_id,
+			trending_date: video.trending_date
 		}));
+	}
+
+	function createBarChartWithApexCharts(data) {
+		const videosData = processDataForBarChartApexCharts(data);
+
+		const options = {
+			chart: {
+				type: 'bar',
+				height: 400,
+				toolbar: {
+					show: false
+				}
+			},
+			plotOptions: {
+				bar: {
+					horizontal: true,
+					distributed: true,
+					dataLabels: {
+						position: 'top'
+					}
+				}
+			},
+			dataLabels: {
+				enabled: false
+			},
+			series: [
+				{
+					data: videosData.map((item) => item.view_count)
+				}
+			],
+			xaxis: {
+				categories: videosData.map((item) => item.title),
+				labels: {
+					show: false
+				}
+			},
+			yaxis: {
+				title: {
+					text: 'Título del Video'
+				}
+			},
+			title: {
+				text: 'Visitas Por Video (ApexCharts)',
+				align: 'center',
+				margin: 20,
+				style: {
+					fontSize: '20px'
+				}
+			},
+			tooltip: {
+				enabled: true,
+				custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+					const title = w.globals.labels[dataPointIndex];
+					const view_count = series[seriesIndex][dataPointIndex];
+					return `<div>Título: ${title}</div><div>Visualizaciones: ${view_count}</div>`;
+				}
+			}
+		};
+
+		const chart = new ApexCharts(document.querySelector('#apexcharts-bar-container'), options);
+		chart.render();
+	}
+
+	function processDataForBarChartApexCharts(data) {
+		const videosData = {};
+		data.forEach((item) => {
+			videosData[item.title] = item.view_count;
+		});
+		return Object.entries(videosData).map(([title, view_count]) => ({ title, view_count }));
 	}
 
 	function showNoDataMessage() {
@@ -147,6 +208,7 @@
 </script>
 
 <svelte:head>
+	<script src="https://code.highcharts.com/highcharts.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 	<title>YouTubeTrends</title>
 </svelte:head>
@@ -156,6 +218,7 @@
 </Container>
 
 <Container id="chart-container">
-	<div id="bar-container" style="width: 80%; height: 300px;"></div>
-	<div id="scatter-container" style="width: 80%; height: 300px;"></div>
+	<div id="highcharts-bar-container" style="width: 80%; height: 300px;"></div>
+	<div id="highcharts-scatter-container" style="width: 80%; height: 300px;"></div>
+	<div id="apexcharts-bar-container" style="width: 80%; height: 500px;"></div>
 </Container>
