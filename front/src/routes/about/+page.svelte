f89da0a7292dfb0939ee1cadde2cfc8bb0921006
NicoRedondoo <redondonico9@gmail.com>
2024-05-17 12:11:05 +0200

    video

--- front/src/routes/about/+page.svelte
+++ front/src/routes/about/+page.svelte
@@ -13,5 +13,6 @@
 	<h1>Vídeos</h1>
 	<ul>
 		<li><a href="https://www.youtube.com/embed/vJf16rV6YBI?si=1q9xEKCPqKYE8j90">Video Víctor Escalera García</a></li>
+        <li><a href="https://www.loom.com/share/a266187220b04de093b1dd673d028c94">Nicolás Redondo Moreno</a></li>
 	</ul>
 </Container>
